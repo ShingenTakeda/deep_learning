69f5107e18f4af42ebeefbe8f2d940e1fe175c5a
GUSTAVO MITSUO FERNANDES VALENTE TAKEDA <gustavomitsuo@tre-ce.jus.br>
2023-10-19 16:42:21 -0300

    Some stuff

--- src/neural_network.hpp
+++ src/neural_network.hpp
@@ -1,5 +1,32 @@
 #pragma once
-//#include <Eigen/Dense>
+#include <vector>
+#include <Eigen/Dense>
 
+//Mutithread this!!!!!
 
-int my_neuron = 0;
+//CHECK THIS SHIT OUT: https://github.com/Cr4ckC4t/neural-network-from-scratch
+
+struct Neuron
+{
+    size_t num_weights;
+    std::vector<double> weights;
+};
+
+struct Layer
+{
+    std::vector<Eigen::MatrixXd> backpropagationLayer;
+    std::vector<Eigen::MatrixXd> layer;
+    std::vector<Eigen::MatrixXd> weights;
+    std::vector<Neuron> neurons;
+};
+
+class NeuralNetework
+{
+    public:
+        NeuralNetework();
+
+        void AddLayer();
+        void UpdateLayer();
+    private:
+        std::vector<Layer> layers;
+};
